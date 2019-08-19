7a2b375d45dc0a93867c0beaeca4e33256435a55
Pavel <pavel.dudkov@icloud.com>
2019-08-19 15:23:42 +0400

    lesson 6 homework done

--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,34 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+import sys
+from pympler import asizeof
+
+
+class Name:
+    def __init__(self, name, age, address, phone):
+        self.name = name
+        self.age = 18
+        self.address = 'Moskau'
+        self.phone = +79999999990
+
+
+class Name_slots:
+    __slots__ = ['name', 'age', 'address', 'phone']
+    def __init__(self, name, age, address, phone):
+        self.name = name
+        self.age = 18
+        self.address = 'Moskau'
+        self.phone = +79999999990
+
+
+name_one = Name('Имя', 21, 'Saratov', 555555)
+name_two = Name_slots('Имя', 21, 'Saratov', 555555)
+
+print(name_one.__dict__)
+print(name_two.__slots__)
+
+print(asizeof.asizeof(name_one))
+print(sys.getsizeof(name_one))
+print(asizeof.asizeof(name_two))
+print(sys.getsizeof(name_two))
