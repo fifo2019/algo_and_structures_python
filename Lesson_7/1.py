d6cc79d551d04aa6a2105124a25bca8d570715a9
Pavel <pavel.dudkov@icloud.com>
2019-08-20 20:49:22 +0400

    lesson 7 homework done

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,28 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+import random
+
+
+def bubbles_sort(n: list):
+    is_sorted = 1
+    for i in range(len(n)):
+        for j in range(len(n)-1):
+            if n[j] < n[j+1]:
+                n[j], n[j+1] = n[j+1], n[j]
+                is_sorted = 0
+        if is_sorted == 1:
+            print('Не сортировался')
+            return n
+    return n
+
+
+test = [random.randint(-100, 100) for i in range(10)]
+test2 = [i for i in range(-10, 0)]
+
+# 1 сортировка по убыванию
+print(bubbles_sort(test))
+
+# 2 сортировка по убыванию сортированного списка
+print(bubbles_sort(test2[::-1]))
--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,41 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+import random
+
+
+def merge_sort(n):
+    if len(n) > 1:
+        c = len(n) // 2
+        left = n[:c]
+        right = n[c:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                n[k] = left[i]
+                i += 1
+            else:
+                n[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            n[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            n[k] = right[j]
+            j += 1
+            k += 1
+        return n
+
+
+test = [random.randint(0, 50) for i in range(10)]
+print(test)
+print(merge_sort(test))
